7591216ab365d622ec6d5dba28c47eab717ab399
alexnerd <alexnerd85@gmail.com>
2023-05-24 22:30:36 +0300

    Add @Schema descriptions to LabelFilterResponse class and fields

--- src/main/java/tech/konso/toolsmanagement/modules/tools/business/label/controller/dto/LabelFilterResponse.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/business/label/controller/dto/LabelFilterResponse.java
@@ -1,5 +1,6 @@
 package tech.konso.toolsmanagement.modules.tools.business.label.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import tech.konso.toolsmanagement.modules.tools.business.label.persistence.dao.Label;
 
 import java.util.List;
@@ -10,5 +11,10 @@ import java.util.List;
  * @param labels     list of found labels. List size limited by page size
  * @param totalItems total number of labels found
  */
-public record LabelFilterResponse(List<Label> labels, Long totalItems) {
+
+@Schema(description = "Response for return result set of labels in pageable format")
+public record LabelFilterResponse(@Schema(description = "list of found labels, list size limited by page size")
+                                  List<Label> labels,
+                                  @Schema(description = "total number of labels found")
+                                  Long totalItems) {
 }
