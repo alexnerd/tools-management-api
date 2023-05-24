04d40640231dbc97244e0673b687d6b3117cad6a
alexnerd <alexnerd85@gmail.com>
2023-05-24 21:54:42 +0300

    Add @Schema descriptions to ToolFilterInfo class fields

--- src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/ToolFilterInfo.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/ToolFilterInfo.java
@@ -1,5 +1,6 @@
 package tech.konso.toolsmanagement.modules.tools.business.tool.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import tech.konso.toolsmanagement.modules.tools.business.tool.persistence.dao.enums.OwnershipType;
 
@@ -34,8 +35,40 @@ import java.util.UUID;
  */
 
 @Builder
-public record ToolFilterInfo(Long id, UUID uuid, String name, Boolean isConsumable, String brand, String inventoryNumber,
-                             String responsible, String category, String project, BigDecimal price, OwnershipType ownershipType,
-                             LocalDate rentTill, Boolean isKit, UUID kitUuid, Set<String> labels, Boolean isArchived,
-                             LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record ToolFilterInfo(@Schema(description = "tool id", example = "1")
+                             Long id,
+                             @Schema(description = "tool business key", example = "3996a6ee-c40e-4401-b8c8-2f96f49d0c22")
+                             UUID uuid,
+                             @Schema(description = "tool name", example = "Makita MTK24")
+                             String name,
+                             @Schema(description = "consumable flag", example = "true")
+                             Boolean isConsumable,
+                             @Schema(description = "brand name", example = "Bosh", nullable = true)
+                             String brand,
+                             @Schema(description = "tool inventory number", example = "0014-HANDTOOL", nullable = true)
+                             String inventoryNumber,
+                             @Schema(description = "responsible person name", example = "Jim Morrison", nullable = true)
+                             String responsible,
+                             @Schema(description = "category name", example = "Handtool", nullable = true)
+                             String category,
+                             @Schema(description = "project name", example = "Some project", nullable = true)
+                             String project,
+                             @Schema(description = "tool price", example = "23400.11", nullable = true)
+                             BigDecimal price,
+                             @Schema(description = "type of ownership", example = "OWN")
+                             OwnershipType ownershipType,
+                             @Schema(description = "last day of tool rent", example = "2024-12-30", nullable = true)
+                             LocalDate rentTill,
+                             @Schema(description = "flag is the tool a kit", example = "false")
+                             Boolean isKit,
+                             @Schema(description = "kit uuid", example = "3996a6ee-c40e-4401-b8c8-2f96f49d0c22", nullable = true)
+                             UUID kitUuid,
+                             @Schema(description = "labels names", example = "[\"Attention\",\"Important\"]")
+                             Set<String> labels,
+                             @Schema(description = "archived flag", example = "false")
+                             Boolean isArchived,
+                             @Schema(description = "create date", example = "false")
+                             LocalDateTime createdAt,
+                             @Schema(description = "update date", example = "false")
+                             LocalDateTime updatedAt) {
 }
