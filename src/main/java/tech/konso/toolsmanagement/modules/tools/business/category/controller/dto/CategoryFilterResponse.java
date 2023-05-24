9505ecf1fe3064520e4b71cd8b87b0fded59a1db
alexnerd <alexnerd85@gmail.com>
2023-05-24 22:37:25 +0300

    Add @Schema descriptions to CategoryFilterResponse class and fields

--- src/main/java/tech/konso/toolsmanagement/modules/tools/business/category/controller/dto/CategoryFilterResponse.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/business/category/controller/dto/CategoryFilterResponse.java
@@ -1,5 +1,6 @@
 package tech.konso.toolsmanagement.modules.tools.business.category.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import tech.konso.toolsmanagement.modules.tools.business.category.persistence.dao.Category;
 
 import java.util.List;
@@ -10,5 +11,10 @@ import java.util.List;
  * @param categories    list of found categories. List size limited by page size
  * @param totalItems    total number of categories found
  */
-public record CategoryFilterResponse(List<Category> categories, Long totalItems) {
+
+@Schema(description = "Response for return result set of categories in pageable format")
+public record CategoryFilterResponse(@Schema(description = "list of found categories, list size limited by page size")
+                                     List<Category> categories,
+                                     @Schema(description = "total number of categories found")
+                                     Long totalItems) {
 }
