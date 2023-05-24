5804386724048bf22c0e9ed7139cc1b4e5f47d1d
alexnerd <alexnerd85@gmail.com>
2023-05-24 23:02:23 +0300

    Add @Schema descriptions to ToolFilterResponse class and correct @Schema description tools field

--- src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/ToolFilterResponse.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/ToolFilterResponse.java
@@ -10,7 +10,9 @@ import java.util.List;
  * @param tools      list of found tools. List size limited by page size
  * @param totalItems total number of tools found
  */
-public record ToolFilterResponse(@Schema(description = "list of found tools. List size limited by page size")
+
+@Schema(description = "Response for return result set of tools in pageable format")
+public record ToolFilterResponse(@Schema(description = "list of found tools, list size limited by page size")
                                  List<ToolFilterInfo> tools,
                                  @Schema(description = "total number of tools found", example = "20")
                                  Long totalItems) {
