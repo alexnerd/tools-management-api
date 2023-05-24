342364d8bb611c8dc05f6fa198e5e5a6494efceb
alexnerd <alexnerd85@gmail.com>
2023-05-24 22:06:14 +0300

    Add @Schema descriptions to LabelShort class and fields

--- src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/LabelShort.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/LabelShort.java
@@ -1,7 +1,12 @@
 package tech.konso.toolsmanagement.modules.tools.business.tool.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record LabelShort(Long id, String name) {
+@Schema(description = "Short label description used to describe tool label by basic fields")
+public record LabelShort(@Schema(description = "label id", example = "1")
+                         Long id,
+                         @Schema(description = "label name", example = "Attention")
+                         String name) {
 }
