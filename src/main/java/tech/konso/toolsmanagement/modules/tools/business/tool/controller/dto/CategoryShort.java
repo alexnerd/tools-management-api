bfb4952687f7d7a094d626df7b9956fbc33837d3
alexnerd <alexnerd85@gmail.com>
2023-05-24 22:09:03 +0300

    Add JavaDoc to CategoryShort class

--- src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/CategoryShort.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/CategoryShort.java
@@ -2,6 +2,14 @@ package tech.konso.toolsmanagement.modules.tools.business.tool.controller.dto;
 
 import lombok.Builder;
 
+/**
+ * Short category description DTO class for {@link ToolInfo}
+ * used in response to describe tool category by basic fields
+ *
+ * @param id     category id
+ * @param name   category name
+ */
+
 @Builder
 public record CategoryShort(Long id, String name) {
 }
