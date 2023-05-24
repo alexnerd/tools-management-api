7f863733b6e76fd49c459a32ad24989dd3f98be4
alexnerd <alexnerd85@gmail.com>
2023-05-24 22:31:53 +0300

    Correct CategoryRequest class code style

--- src/main/java/tech/konso/toolsmanagement/modules/tools/business/category/controller/dto/CategoryRequest.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/business/category/controller/dto/CategoryRequest.java
@@ -1,6 +1,5 @@
 package tech.konso.toolsmanagement.modules.tools.business.category.controller.dto;
 
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,6 +10,7 @@ import jakarta.validation.constraints.NotNull;
  * @param name       of the category, must not be blank
  * @param isArchived flag, must not be null
  */
+
 public record CategoryRequest(@Schema(description = "category name", example = "Hand tools")
                               @NotBlank String name,
                               @Schema(description = "archived flag", example = "false")
