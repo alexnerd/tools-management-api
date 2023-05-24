730441c9a111f1d4ee32f6ab366f57cf6dabdce3
alexnerd <alexnerd85@gmail.com>
2023-05-24 22:07:10 +0300

    Add JavaDoc to LabelShort class

--- src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/LabelShort.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/LabelShort.java
@@ -3,6 +3,14 @@ package tech.konso.toolsmanagement.modules.tools.business.tool.controller.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+/**
+ * Short label description DTO class for {@link ToolInfo}
+ * used in response to describe tool label by basic fields
+ *
+ * @param id     label id
+ * @param name   label name
+ */
+
 @Builder
 @Schema(description = "Short label description used to describe tool label by basic fields")
 public record LabelShort(@Schema(description = "label id", example = "1")
