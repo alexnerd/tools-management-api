988d20425ba8f808e0d372c4f36d83665adeb968
alexnerd <alexnerd85@gmail.com>
2023-05-24 22:20:01 +0300

    Correct LabelRequest class code style

--- src/main/java/tech/konso/toolsmanagement/modules/tools/business/label/controller/dto/LabelRequest.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/business/label/controller/dto/LabelRequest.java
@@ -1,6 +1,5 @@
 package tech.konso.toolsmanagement.modules.tools.business.label.controller.dto;
 
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,6 +10,7 @@ import jakarta.validation.constraints.NotNull;
  * @param name       of the label, must not be blank
  * @param isArchived flag, must not be null
  */
+
 public record LabelRequest(@Schema(description = "label name", example = "Attention")
                            @NotBlank String name,
                            @Schema(description = "archived flag", example = "false")
