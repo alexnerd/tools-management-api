b06cd3b18abdac23c19d2abaaab039394aaea650
alexnerd <alexnerd85@gmail.com>
2023-05-24 22:14:57 +0300

    Add JavaDoc to BrandShort class

--- src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/BrandShort.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/business/tool/controller/dto/BrandShort.java
@@ -2,6 +2,14 @@ package tech.konso.toolsmanagement.modules.tools.business.tool.controller.dto;
 
 import lombok.Builder;
 
+/**
+ * Short brand description DTO class for {@link ToolInfo}
+ * used in response to describe tool brand by basic fields
+ *
+ * @param id     brand id
+ * @param name   brand name
+ */
+
 @Builder
 public record BrandShort(Long id, String name) {
 }
