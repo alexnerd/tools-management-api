e9923cec9a39ceb6063ee631c31257a385440a08
alexnerd <alexnerd85@gmail.com>
2023-07-29 12:05:49 +0300

    Removed unnecessary line break

--- src/main/java/tech/konso/toolsmanagement/modules/tools/commons/exceptions/BPException.java
+++ src/main/java/tech/konso/toolsmanagement/modules/tools/commons/exceptions/BPException.java
@@ -1,7 +1,6 @@
 package tech.konso.toolsmanagement.modules.tools.commons.exceptions;
 
 public class BPException extends RuntimeException {
-
     public BPException(String message) {
         super(message);
     }
